f565e29eedf249b333e718bbae93c274760882cd
Alonso Project <alonso.project@outlook.com>
2020-05-09 21:24:40 +0800

    Added Config

---
+++ core/Config.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+class Config
+{
+public:
+	static inline int width = 800;
+	static inline int height = 600;
+	static inline std::string appName = "Game";
+
+	static inline int rowBoardNumber = 8;
+	static inline int colomnBoardNumber = 8;
+};
