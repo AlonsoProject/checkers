140bbb2355a8838c0017c561989ee0762e62ec00
Alonso Project <alonso.project@outlook.com>
2020-05-09 21:26:59 +0800

    Added Tile

---
+++ graphics/Tile.cpp
@@ -0,0 +1,38 @@
+#include "Tile.hpp"
+#include "../core/ComponentManager.hpp"
+
+Tile::Tile(const std::string& pathToTile) :
+tileTexture(new sf::Texture), tileSprite(new sf::Sprite)
+{
+	tileTexture->loadFromFile(pathToTile);
+	tileSprite->setTexture(*tileTexture);
+}
+
+Tile::~Tile()
+{
+	if (tileTexture) delete tileTexture;
+	if (tileSprite) delete tileSprite;
+}
+
+void Tile::resize(uint32_t width, uint32_t height)
+{
+	tileSprite->setScale(
+		width	/ tileSprite->getLocalBounds().width,
+		height	/ tileSprite->getLocalBounds().height
+	);
+}
+
+void Tile::draw()
+{
+	ComponentManager::get()->getWindow()->draw(*tileSprite);
+}
+
+void Tile::move(const sf::Vector2f& offset)
+{
+	tileSprite->move(offset);
+}
+
+void Tile::setPosition(const sf::Vector2f& position)
+{
+	tileSprite->setPosition(position);
+}
---
+++ graphics/Tile.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include <string>
+
+#include "../core/IDrawable.hpp"
+#include "../core/IMoveable.hpp"
+
+class ComponentManager;
+
+class Tile : public IDrawable, public IMoveable
+{
+private:
+	sf::Texture* tileTexture;
+	sf::Sprite* tileSprite;
+public:
+	Tile(const std::string& pathToTile);
+	~Tile();
+
+	void resize(uint32_t width, uint32_t height);
+
+	virtual void draw() override;
+
+	virtual void move(const sf::Vector2f& offset) override;
+	virtual void setPosition(const sf::Vector2f& position) override;
+};
