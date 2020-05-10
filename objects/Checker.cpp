337236755d0f74116b057b8214a655a4143cdbc2
Alonso Project <alonso.project@outlook.com>
2020-05-10 23:07:11 +0800

    Update constructor

--- objects/Checker.cpp
+++ objects/Checker.cpp
@@ -1,14 +1,19 @@
 #include "Checker.hpp"
 
-Checker::Checker(std::string pathToCheckerTile, sf::Vector2f position) :
-checkerTile(new Tile(pathToCheckerTile))
+Checker::Checker(int idOwner, std::string pathToCheckerTile, sf::Vector2f position) :
+idOwner(idOwner), checkerTile(new Tile(pathToCheckerTile))
 {
-	checkerTile->setPosition(position);
+	setPosition(position);
 }
 
 Checker::~Checker()
 {
 	if (checkerTile) delete checkerTile;
+}
+
+int Checker::getIdOwner()
+{
+	return idOwner;
 }
 
 Tile* Checker::getCheckerTile()
@@ -19,4 +24,14 @@ Tile* Checker::getCheckerTile()
 void Checker::draw()
 {
 	checkerTile->draw();
+}
+
+void Checker::move(const sf::Vector2f& offset)
+{
+	checkerTile->move(offset);
+}
+
+void Checker::setPosition(const sf::Vector2f& position)
+{
+	checkerTile->setPosition(position);
 }
--- objects/Checker.hpp
+++ objects/Checker.hpp
@@ -2,17 +2,23 @@
 
 #include "../graphics/Tile.hpp"
 #include "../core/IDrawable.hpp"
+#include "../core/IMoveable.hpp"
 
-class Checker : public IDrawable
+class Checker : public IDrawable, public IMoveable
 {
 private:
 	int idOwner;
 	Tile* checkerTile;
 public:
-	Checker(std::string pathToCheckerTile, sf::Vector2f position);
+	Checker(int idOwner, std::string pathToCheckerTile, sf::Vector2f position);
 	~Checker();
+
+	int getIdOwner();
 
 	Tile* getCheckerTile();
 
 	virtual void draw() override;
+
+	virtual void move(const sf::Vector2f& offset) override;
+	virtual void setPosition(const sf::Vector2f& position) override;
 };
