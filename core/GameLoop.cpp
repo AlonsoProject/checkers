5d6599589d3b2ec385d102fcba9a558f1c2acd7e
Alonso Project <alonso.project@outlook.com>
2020-05-11 13:46:23 +0800

    Added tmp solution

--- core/GameLoop.cpp
+++ core/GameLoop.cpp
@@ -6,13 +6,42 @@ void GameLoop::run()
 
 	sf::Event event;
 
+	std::string currentUserName = GameObjectManager::get()->
+												 getPlayerController()->
+												 getCurrentPlayer()->
+												 getName();
+
+	Text userInGame(currentUserName, Config::defaultFont);
+	userInGame.setPosition(sf::Vector2f(
+		10,
+		10
+	));
+	userInGame.getInstance()->setFillColor(sf::Color::Blue);
+
 	GameObjectManager::get()->getGameSession()->resetTimer();
+
+	int time = GameObjectManager::get()->getGameSession()->getCountdown();
+	Text timer(std::to_string(time), Config::defaultFont);
+	timer.setPosition(sf::Vector2f(
+		Config::width * 0.93,
+		10
+	));
+	timer.getInstance()->setFillColor(sf::Color::Blue);
+
 	while (true)
 	{
 		if (GameObjectManager::get()->getGameSession()->isExpired())
 		{
+			GameObjectManager::get()->getBoard()->clearHighlightedCells();
 			GameObjectManager::get()->getPlayerController()->togglePlayer();
 			GameObjectManager::get()->getGameSession()->resetTimer();
+
+			currentUserName = GameObjectManager::get()->
+											 getPlayerController()->
+											 getCurrentPlayer()->
+											 getName();
+
+			userInGame.getInstance()->setString(currentUserName);
 		}
 
 		while (ServiceObjectManager::get()->getWindow()->pollEvent(event))
@@ -20,7 +49,12 @@ void GameLoop::run()
 			eventController.handle(event);
 		}
 
+		time = GameObjectManager::get()->getGameSession()->getCountdown();
+		timer.getInstance()->setString(std::to_string(time));
+
 		GameObjectManager::get()->getBoard()->draw();
+		userInGame.draw();
+		timer.draw();
 		ServiceObjectManager::get()->getWindow()->display();
 		ServiceObjectManager::get()->getWindow()->clear();
 	}
